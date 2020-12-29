2d799ee1a7e0aec8c68a193a63b904e07fffca93
SunnySparks <josediazt6@outlook.com>
2020-12-28 20:55:41 -0600

    Migration of create posts

--- instagram-clone/app/controllers/accounts_controller.rb
+++ instagram-clone/app/controllers/accounts_controller.rb
@@ -1,2 +1,11 @@
 class AccountsController < ApplicationController
+    before_action :authenticate_account!
+
+    def index
+        # user feed
+    end
+
+    def show
+        # user profile
+    end
 end
---
+++ instagram-clone/db/migrate/20201229025509_create_posts.rb
@@ -0,0 +1,6 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+    end
+  end
+end
