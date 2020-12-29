3a9695ed23bc0fd7be64961ddd89d2e01c3ec8aa
SunnySparks <josediazt6@outlook.com>
2020-12-28 21:37:01 -0600

    create image uploader

--- instagram-clone/db/migrate/20201229025509_create_posts.rb
+++ instagram-clone/db/migrate/20201229025509_create_posts.rb
@@ -1,6 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
+      t.string :image
+      t.boolean :active
+      t.references :acocunt
+      t.timestamps
     end
   end
 end
--- instagram-clone/db/schema.rb
+++ instagram-clone/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_011002) do
+ActiveRecord::Schema.define(version: 2020_12_29_025509) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2020_12_29_011002) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "image"
+    t.boolean "active"
+    t.integer "acocunt_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["acocunt_id"], name: "index_posts_on_acocunt_id"
   end
 
 end
