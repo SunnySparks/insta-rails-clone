ffd1842581b5ca3cbb036fc46a7eb101521580ff
SunnySparks <josediazt6@outlook.com>
2020-12-28 20:43:32 -0600

    create accounts for rails

--- instagram-clone/config/routes.rb
+++ instagram-clone/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # dashboard
+  get "/dashboard" => "accounts#index"
+
   root to: "public#homepage"
 
 end
